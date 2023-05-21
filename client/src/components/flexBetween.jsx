6c7461219327d146fcd33e1bc2197cee6eb38243
ezitounioussama <zitounioussama95@gmail.com>
2023-05-22 00:29:51 +0100

    recovering my first importation

--- client/src/components/flexBetween.jsx
+++ client/src/components/flexBetween.jsx
@@ -1,7 +1,6 @@
-// const { Box } = require("@mui/material");
-// const { styled } = require("@mui/system");
-import { Box } from "@mui/material";
-import { styled } from "@mui/system";
+const { Box } = require("@mui/material");
+const { styled } = require("@mui/system");
+
 const FlexBetween = styled(Box)({
   display: "flex",
   justifyContent: "space-between",
