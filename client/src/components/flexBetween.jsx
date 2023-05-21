60525bd883e6d411ad8f5931d6d0b34f95a65147
ezitounioussama <zitounioussama95@gmail.com>
2023-05-22 00:22:31 +0100

    testing import solution by my intellegent mind 😁

--- client/src/components/flexBetween.jsx
+++ client/src/components/flexBetween.jsx
@@ -1,6 +1,7 @@
-const { Box } = require("@mui/material");
-const { styled } = require("@mui/system");
-
+// const { Box } = require("@mui/material");
+// const { styled } = require("@mui/system");
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
 const FlexBetween = styled(Box)({
   display: "flex",
   justifyContent: "space-between",
