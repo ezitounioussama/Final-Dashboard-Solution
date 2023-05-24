9e1f0a403702b0ce984463d5b66167971555f528
ezitounioussama <zitounioussama95@gmail.com>
2023-05-24 12:54:14 +0100

    works in local

--- client/src/components/flexBetween.jsx
+++ client/src/components/flexBetween.jsx
@@ -1,5 +1,8 @@
-import Box from "@mui/material";
-import styled from "@mui/system";
+import { Box } from "@mui/material";
+import { styled } from "@mui/system";
+
+// const { Box } = require("@mui/material");
+// const { styled } = require("@mui/system");
 
 const FlexBetween = styled(Box)({
   display: "flex",
